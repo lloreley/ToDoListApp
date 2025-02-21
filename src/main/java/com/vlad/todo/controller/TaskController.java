52cfff73c8f337ae748cdc9822e7a1ff6929caf4
lloreley <supervoin.d1@gmail.com>
2025-02-21 12:46:56 +0300

    Ready to first lab

--- src/main/java/com/vlad/todo/controller/TaskController.java
+++ src/main/java/com/vlad/todo/controller/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/tasks")
+@RequestMapping("/tasks")
 public class TaskController {
     private final TaskService taskService;
 
